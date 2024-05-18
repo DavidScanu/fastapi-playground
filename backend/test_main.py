a9d1c9cb87dd23434c0170d6f8cb7a8a6b769a56
David Scanu <DavidScanu@users.noreply.github.com>
2024-05-18 09:45:59 +0000

    Added .env to docker-compose.yaml + test_main.py

--- backend/test_main.py
+++ backend/test_main.py
@@ -1,32 +1,15 @@
 import psycopg
 from unittest import TestCase, main
 import os
-from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
-
-# DEFINE THE DATABASE CREDENTIALS
-user = os.environ['DB_USER']
-password = os.environ['DB_PASS']
-host = os.environ['DB_HOST']
-port = os.environ['DB_PORT']
-database = os.environ['DB_NAME']
-
-
-db_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-
-print(db_url)
 
 class TestDatabase(TestCase):
     # Vérifier connection à la base de données
     def test_connection(self):
 
-        # DB_PSY_URI = st.secrets.DB_PSY_URI
-        db_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-        print(db_url)
-        self.assertIsNotNone(db_url)
+        DB_URI = os.environ['DB_URI']
+        self.assertIsNotNone(DB_URI)
 
-        with psycopg.connect(conninfo=db_url) as conn:
+        with psycopg.connect(conninfo=DB_URI) as conn:
             self.assertIsInstance(conn, psycopg.Connection)
 
 if __name__  == '__main__':
