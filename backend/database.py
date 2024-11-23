2c5b85081d52b23a3e05041eb120734a5ca0e03f
David Scanu <davidscanu14@gmail.com>
2024-11-23 19:10:58 +0100

    Major Update : SQL Model with PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,7 @@
 import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
 POSTGRES_USER = os.environ['POSTGRES_USER']
 POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
@@ -9,5 +12,21 @@ POSTGRES_DB = os.environ['POSTGRES_DB']
 def get_db_uri() -> str:
     return f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-def get_db_uri_psy() -> str:
+def get_db_uri_sqlalchemy() -> str:
     return f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+# SQLALCHEMY_DATABASE_URL = get_db_uri_sqlalchemy()
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
